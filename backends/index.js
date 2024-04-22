fa382fd37e560d37cafa1c6bbfdf30c6726bee28
namnhi-dev <namnhi.mit@gmail.com>
2024-04-22 14:43:35 +0700

    update server

--- backends/index.js
+++ backends/index.js
@@ -1,1 +1,7 @@
-console.log("hihi");
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
